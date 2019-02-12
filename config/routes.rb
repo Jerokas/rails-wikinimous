f11a84c6249561c026b3a8a75148545ad45cb13d
Jerome Kastoun <jeromekastoun@gmail.com>
2019-02-12 17:08:53 -0500

    working on bugs in the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
+  resources :articles, only: [:create, :index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+require 'faker'
+
+puts 'Creating 100 fake article..'
+10.times do
+  article = Article.new(
+    title:    Faker::Company.name,
+    content: "#{Faker::Address.street_address}, #{Faker::Address.city}"
+  )
+  article.save!
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
